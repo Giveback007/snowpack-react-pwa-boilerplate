3ea519c8930e11ecf84eb985f37188b0b861c4e6
giveback007 <dovy91@gmail.com>
2020-12-03 04:38:16 -0500

    added index.d.ts

--- copy-files/src/index.tsx
+++ copy-files/src/index.tsx
@@ -19,6 +19,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
+// if (import.meta.hot) 
+import.meta.hot?.accept();
